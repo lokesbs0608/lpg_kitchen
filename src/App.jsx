0ab0347f1b60157857e6a1ffbf9ce5ca91443f59
fourmbdotcom <84077131+lokesbs0608@users.noreply.github.com>
2023-03-01 02:31:32 +0530

    dataset

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import * as React from 'react';
 import ResponsiveAppBar from './Components/Header/Header';
 import Navigation from './Naviagtion/Naviagation';
 import Footer from './Components/Footer/Footer'
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 function App() {
   return (
--- src/Components/MenuCard/MenuCard.tsx
+++ src/Components/MenuCard/MenuCard.tsx
@@ -1,28 +1,64 @@
-import React from 'react'
+import * as  React from 'react'
 import Styles from './MenuCard.module.scss'
-import { Checkbox } from '@mui/material'
+import Swiggy from '../../assets/Logo/Swiggy-logo-png.webp';
+import data from '../../data/MenuList.json'
 
+
+
+interface Props {
+    title: string;
+    price: number;
+    image: string;
+    description: string;
+}
 
 const MenuCard = () => {
 
-    const MenuList = () => {
+    const MenuList = ({ title, price, image, description }: Props) => {
         return (
-            <div className={Styles.card_container} >
-                <div className={Styles.inner_container} >
-                    <div className={Styles.check_box_conatiner} > <Checkbox  defaultChecked /></div>
-                    <div className={Styles.description_conatienr}>
-                        <div>title</div>
-                        <div>desciption / Price</div>
+            <div className="container mt-5">
+                <div className="menu-items mt-5">
+                    <div className="card mb-3 col-md-6" id="breakfast">
+                        <div className="row g-0">
+                            <div className="col-md-4 col-sm-12">
+                                <img  src={Swiggy} className="img-fluid rounded-start border" alt="..."/>
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <div className="card-title">
+                                        <h5 className="d-inline mt-5 mr-4">{title}</h5>
+                                        <span className="price text-warning m4-4">{price}</span>
+                                    </div>
+                                    <p className="card-text">{description}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className={Styles.image_conatiner}>image</div>
+
                 </div>
             </div>
+
         )
     }
 
+
+
+
     return (
         <div className={Styles.menu_outer_conatiner}>
-            <MenuList/>
+            {data.menuList.map((item)=>{
+                return(
+                    <div>
+                        <div>{item.title}</div>
+                        {item.menu.map((ele)=>{
+                            return(
+                                <MenuList  key={ele.id} title={ele.title} price={ele.price} image={''} description={ele.description} />
+                            )
+                        })}
+                    </div>
+                )
+            })}
+        
         </div>
     )
 }
